69fdc8386167cde8fc04170428313b9ae4f5b4e3
Sam Adams <samuel.fitz@btinternet.com>
2022-04-20 16:59:02 +0100

    Routes, Layouts and Initial Pages Added

---
+++ components/Navbar.js
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="logo">
+        <h1>Ninjas List</h1>
+      </div>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+      <Link href="/ninjas">
+        <a>Ninja List</a>
+      </Link>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ pages/about.js
@@ -0,0 +1,21 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+        dicta tempore aspernatur doloribus, officia nostrum, nihil perferendis
+        sit qui, a et quasi necessitatibus fugiat culpa dolor asperiores
+        suscipit perspiciatis! Consectetur?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi
+        consequatur quia pariatur voluptatum placeat minus quis nam culpa unde!
+        Facere delectus dicta suscipit est consectetur beatae harum ratione
+        aperiam aliquid.
+      </p>
+    </div>
+  );
+};
+
+export default About;
